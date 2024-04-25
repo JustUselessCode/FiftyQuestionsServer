bb98a631c1d72f7b8d928d65873cfac9c9236363
Tim.Zander <tim.zander@edu.siemens-energy.com>
2024-04-25 22:54:08 +0200

    Added implementations for Buzzing and Game Room Creation.

---
+++ Entities/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace FiftyQuestionsServer.Entities;
+
+public class Player
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public bool Winner { get; set; }
+
+    public PlayerRole _Role { get; set; }
+
+    public Player(string name, PlayerRole role)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        _Role = role;
+        Winner = false;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append("Name: " + Name);
+        sb.Append("  Id: " + Id.ToString());
+        return sb.ToString();
+    }
+}
